33ff40ce41d90312892ba42d35568e6b80df1f37
Jessica Pereira <jcpereira070@gmail.com>
2020-06-21 17:30:49 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -46,12 +46,7 @@ holiday_hash[season][holiday_name] = supply_array
 end
 
 def all_winter_holiday_supplies(holiday_hash)
-  holiday_hash.each do |season, _holiday_hash|
-  puts "#{season.to_s.capitalize}:"
-  _holiday_hash.each do |holiday, supplies|
-    capitalized_holiday = holiday.to_s.split('_').each {|i| i.capitalize!}.join(' ')
-    puts "  #{capitalized_holiday}: #{supplies.join(', ')}"
-  end
+  holiday_hash[:winter].values.flatten
 end
   # return an array of all of the supplies that are used in the winter season
 
